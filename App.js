d156f64ed60e1bad707d37028c262ff6c5811f0f
Aashish-Dahal <ashishdahal490@gmail.com>
2022-07-19 15:03:31 +0545

    Material Top Tabs Navigator

--- App.js
+++ App.js
@@ -11,69 +11,23 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import Home from './src/navigation/screens/Home';
 import Wishlist from './src/navigation/screens/wishlist';
 import Cart from './src/navigation/screens/cart';
 import Profile from './src/navigation/screens/Profile';
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: 'red',
-          },
-          tabBarStyle: {
-            backgroundColor: 'white',
-          },
-          tabBarLabelStyle: {
-            fontWeight: 'bold',
-          },
-        }}>
-        <Tab.Screen
-          name="Home"
-          component={Home}
-          options={{
-            tabBarIcon: () => (
-              <MaterialCommunityIcons name="home" color="white" size={20} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Wishlist"
-          component={Wishlist}
-          options={{
-            tabBarIcon: () => (
-              <MaterialCommunityIcons
-                name="cards-heart"
-                color="white"
-                size={20}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Cart"
-          component={Cart}
-          options={{
-            tabBarIcon: () => (
-              <MaterialCommunityIcons name="cart" color="white" size={20} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={Profile}
-          options={{
-            tabBarIcon: () => (
-              <MaterialCommunityIcons name="account" color="white" size={20} />
-            ),
-          }}
-        />
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Wishlist" component={Wishlist} />
+
+        <Tab.Screen name="Cart" component={Home} />
+
+        <Tab.Screen name="Profile" component={Home} />
       </Tab.Navigator>
     </NavigationContainer>
   );
