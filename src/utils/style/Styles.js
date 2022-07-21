c312cba8124bc5b16537df9f29d55fefac58b790
Aashish-Dahal <ashishdahal490@gmail.com>
2022-07-21 13:28:46 +0545

    fix: App Styles

--- src/utils/style/Styles.js
+++ src/utils/style/Styles.js
@@ -6,12 +6,18 @@ export const styles = StyleSheet.create({
   gradient: {
     flex: 1,
   },
+  icon: {
+    marginLeft: 10,
+    marginTop: 10,
+    marginRight: 5,
+  },
   inputCard: {
-    marginTop: 15,
+    marginTop: 10,
     flexDirection: 'row',
     backgroundColor: '#C7CDEC',
     width: '80%',
     height: 45,
+    marginBottom: 0,
     borderColor: 'white',
     borderWidth: 1,
     borderRadius: 8,
@@ -19,21 +25,22 @@ export const styles = StyleSheet.create({
 
   mainContainer: {
     marginTop: 70,
+
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
   },
   heading: {
-    marginTop: 40,
     fontSize: 40,
     color: 'white',
     fontWeight: 'bold',
     textAlign: 'center',
-    marginBottom: 20,
+    marginTop: 15,
+    marginBottom: 5,
   },
   gradientButton: {
     marginBottom: 20,
-    marginTop: 40,
+    marginTop: 80,
     borderRadius: 100,
     elevation: 5,
     width: 250,
@@ -50,11 +57,41 @@ export const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  bodySmall: {
+  subHeading: {
     fontSize: 20,
     textAlign: 'center',
+    color: 'white',
+    marginBottom: 30,
+  },
+  checkBoxTile: {
+    flexDirection: 'row',
+    alignContent: 'space-between',
+    justifyContent: 'space-between',
+  },
+  trailing: {
+    marginTop: 18,
+    color: 'white',
+    fontSize: 14,
+    marginLeft: 50,
+    fontWeight: 'bold',
+  },
+  text: {
+    fontSize: 16,
 
     color: 'white',
-    marginBottom: 20,
+  },
+  subText: {
+    marginLeft: 2,
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  checkBoxContainer: {
+    marginVertical: 0,
+
+    marginTop: 5,
+    marginHorizontal: 0,
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
   },
 });
