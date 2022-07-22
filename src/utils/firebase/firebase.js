c156a25c9e1ac60dddb6292e3237871f8b570ab5
Aashish-Dahal <ashishdahal490@gmail.com>
2022-07-22 16:20:59 +0545

    Firebase setup

---
+++ src/utils/firebase/firebase.js
@@ -0,0 +1,12 @@
+import auth from '@react-native-firebase/auth';
+export default class FirebaseUtil {
+  static sigIn = (email, password) => {
+    return auth().signInWithEmailAndPassword(email, password);
+  };
+  static signUp = (email, password) => {
+    return auth().createUserWithEmailAndPassword(email, password);
+  };
+  static signOut = () => {
+    return auth().signOut();
+  };
+}
