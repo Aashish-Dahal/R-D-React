0f4a14434dfa91b98f75319847971ff8bc9011f6
Aashish-Dahal <ashishdahal490@gmail.com>
2022-07-22 16:24:37 +0545

    Firebase Signup

--- src/components/screens/RegisterComponent.js
+++ src/components/screens/RegisterComponent.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {Text, View, TouchableOpacity} from 'react-native';
+import {ActivityIndicator, Text, View} from 'react-native';
 import * as Color from '../../utils/style/Color';
 import {styles} from '../../utils/style/Styles';
 import LinearGradient from 'react-native-linear-gradient';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import ImageBox from '../customComponents/ImageBox';
 import RichText from '../customComponents/RichText';
 import InputField from '../customComponents/InputField';
 import GradientButton from '../customComponents/GradientButton';
+import FirebaseUtil from '../../utils/firebase/firebase';
+import {useForm} from 'react-hook-form';
 
 export default function RegisterScreen({navigation}) {
+  const [loading, setLoading] = useState(false);
+
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm();
+  const handleRegister = async data => {
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    await FirebaseUtil.signUp(data.email, data.password).catch(e => {
+      console.log(e);
+
+      alert('something wrong');
+    });
+    setLoading(false);
+  };
   return (
     <LinearGradient
       colors={[Color.darkBlue, Color.lightBlue]}
@@ -23,20 +43,41 @@ export default function RegisterScreen({navigation}) {
           heading="Register"
           subHeading="Create your account"
         />
-        <InputField iconName="account" placeholder="Username"></InputField>
         <InputField
+          rules={{required: 'Username is required'}}
+          name="username"
+          control={control}
+          iconName="account"
+          placeholder="Username"></InputField>
+        <InputField
+          rules={{required: 'Email is required'}}
+          name="email"
+          control={control}
           iconName="email-outline"
           placeholder="Email address"></InputField>
-        <InputField iconName="lock-outline" placeholder="Password"></InputField>
         <InputField
+          rules={{required: 'Password is required'}}
+          name="password"
+          control={control}
+          iconName="lock-outline"
+          placeholder="Password"></InputField>
+        <InputField
+          name="cp"
+          control={control}
           iconName="lock-outline"
           placeholder="Confirm password"></InputField>
         <Text style={{fontSize: 14, textAlign: 'center', marginTop: 10}}>
           By registering,you are agreeing to our Terms of use and{'\n'} Privacy
           Policy.
         </Text>
-
-        <GradientButton text="REGISTER" />
+        {loading ? (
+          <ActivityIndicator size="large" color="red"></ActivityIndicator>
+        ) : (
+          <GradientButton
+            text="REGISTER"
+            onPress={handleSubmit(handleRegister)}
+          />
+        )}
         <RichText
           onPress={() => navigation.navigate('Login')}
           disabled={false}
